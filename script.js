45b2d4fcd46574d8fafa8722f20913d759858cd3
sivaSai9177 <57568614+sivaSai9177@users.noreply.github.com>
2019-11-14 19:48:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var leftContainer = document.querySelector('.left');
+var rightContainer = document.querySelector('.right');
+var placeholder = document.querySelectorAll('.dist');
+var delayContainer = document.querySelectorAll('.anime2');
+var place = document.querySelectorAll('.place');
+var menu = document.querySelector('.menu');
+
+
+
+
+
+var menuWrapper = document.querySelector('.menu-wrapper');
+var hamburgerMenu = document.querySelector('.hamburger-menu');
+var animationPage = document.querySelector('.animation-page');
+
+menuWrapper.addEventListener('click', function () {
+    console.log(delayContainer);
+    hamburgerMenu.classList.toggle('animate');
+    animationPage.classList.toggle('reverse');
+    leftContainer.classList.toggle('anime');
+    rightContainer.classList.toggle('anime');
+
+    placeholder.forEach(holder => {
+        holder.classList.toggle('anime2');
+    });
+    if (animationPage.classList.contains('reverse')) {
+        placeholder.forEach(holder => {
+            holder.style.transition = 'opacity 0.3 s 0.2 s ease';
+        })
+    }
+});
+
